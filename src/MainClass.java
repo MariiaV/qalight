cd93da9c2ca7be74fb1bf09212331c99be118403
MariiaV <mariya.petrina@gmail.com>
2018-02-28 22:41:57 +0200

    Homework1

---
+++ src/MainClass.java
@@ -0,0 +1,30 @@
+import libs.Calc;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        int var_1;
+        var_1 = 5;
+        int var_2 = 10;
+        System.out.println(var_1 + var_2);
+//        int rez = var_1 + var_2;
+//        System.out.println("Rez =" + rez);
+        Calc.sum(var_1, var_2);
+
+
+        int var_3 = 15;
+        //System.out.println("Rez = " + (var_1 + var_3));
+
+        Calc.sum(var_1, var_3);
+        Calc.negate(var_1, var_3);
+        Calc.multiply(var_1, var_3);
+
+
+
+
+    }
+
+}
---
+++ src/libs/Calc.java
@@ -0,0 +1,16 @@
+package libs;
+
+public class Calc {
+
+    public static void sum(int tempVar1, int tempVar2) {
+        System.out.println("Rez = " + (tempVar1 + tempVar2));
+    }
+
+    public static void negate(int tempVar1, int tempVar2) {
+        System.out.println("Rez = " + (tempVar1 - tempVar2));
+    }
+
+    public static void multiply(int tempVar1, int tempVar2) {
+        System.out.println("Rez = " + (tempVar1 * tempVar2));
+    }
+}
