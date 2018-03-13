b0db7e914ff4374b6fd7ac76736e5b69c92d3435
MariiaV <mariya.petrina@gmail.com>
2018-03-13 15:02:44 +0200

    Homework2

--- src/MainClass.java
+++ src/MainClass.java
@@ -24,8 +24,23 @@ public class MainClass {
         Calc.division(var_2, var_1);
         Calc.division((double) var_2, (double) var_1);
         int[][] arrayArray = new int[3][3];
-        WorkWithArray.fillingOFDiagonal(arrayArray);
-        WorkWithArray.fillingOFDiagonalright(arrayArray);
+        WorkWithArray workWithArray = new WorkWithArray();
+
+
+
+
+
+        // WorkWithArray.filld (arrayArray, 1);
+        int[][] arrayArray1 = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+        workWithArray.fillingOFDiagonal(arrayArray, 8);
+        workWithArray.fillingOFDiagonal(arrayArray, 4);
+        int[][] emptyArray = new int[0][0];
+        int[][] rezMatrix2 = workWithArray.fillingOFDiagonal(emptyArray, 6);
+
+
+
+        System.out.println("-- end --");
 
 
     }
--- src/WorkWithArray.java
+++ src/WorkWithArray.java
@@ -1,23 +1,85 @@
 public class WorkWithArray {
 
-    public static void fillingOFDiagonal(int [][] arrayArray) {
+    public int [][] fillingOFDiagonal(int[][] arrayArray, int value) {
+        try {
 
 
+            if (arrayArray.length == arrayArray[0].length) {
+                if ((arrayArray.length > 0) && (arrayArray[0].length > 0)) {
+                    //    int[][] arrayArray = new int[3][3];
+                    for (int i = 0; i < arrayArray.length; i++) {
+                        for (int j = 0; j < arrayArray[i].length; j++) {
+                            if (i == j) {
+                                arrayArray[i][j] = value;
+                            }
 
-        for (int i = 0; i < arrayArray.length; i++) {
-            for (int j = 0; j < arrayArray[i].length; j++) {
-                if (i == 0 && j == 0) {
-                    arrayArray[i][j] = 1;
-                } else if (i == 1 && j == 1) {
-                    arrayArray[i][j] = 1;
-                } else if (i == 2 && j == 2) {
-                    arrayArray[i][j] = 1;
+
+                            //  System.out.print(arrayArray[i][j]);
+                        }
+
+
+                        // System.out.println("");
+                        printArray(arrayArray);
+                    }
                 } else {
-                    arrayArray[i][j] = 0;
+                    System.out.println("length <= 0");
+
                 }
+            } else {
+                System.out.println(" Lenght should be the same ");
+            }
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+        return arrayArray;
+    }
+
+    public void fillingOFDiagonal(String[][] arrayArray, int value) {
+        try {
 
 
-                System.out.print(" " + arrayArray[i][j]);
+            if (arrayArray.length == arrayArray[0].length) {
+                if ((arrayArray.length > 0) && (arrayArray[0].length > 0)) {
+                    //    int[][] arrayArray = new int[3][3];
+                    for (int i = 0; i < arrayArray.length; i++) {
+                        for (int j = 0; j < arrayArray[i].length; j++) {
+                            if (i == j) {
+                                arrayArray[i][j] = String.valueOf( value);
+                            }
+
+
+                            //  System.out.print(arrayArray[i][j]);
+                        }
+
+
+                        // System.out.println("");
+                        printArray(arrayArray);
+                    }
+                } else {
+                    System.out.println("length <= 0");
+
+                }
+            } else {
+                System.out.println(" Lenght should be the same ");
+            }
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+    }
+
+    private void printArray (int [][] ourArray){
+        for (int i = 0; i < ourArray.length ; i++) {
+            for (int j = 0; j < ourArray[0].length ; j++) {
+                System.out.print(ourArray[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+
+    private void printArray(String[][] ourArray) {
+        for (int i = 0; i < ourArray.length; i++) {
+            for (int j = 0; j < ourArray[0].length; j++) {
+                System.out.print(ourArray[i][j]);
             }
             System.out.println("");
         }
